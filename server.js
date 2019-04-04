3fc8d6667d7d4fc12f3b0b9b68cdbc2de3846237
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-04-04 11:37:04 +0530

    Config change for production

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/todo-list'));
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/todo-list/index.js'));
+});
